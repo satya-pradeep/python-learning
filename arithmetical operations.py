21062e323e925b35bd6111cd2bb42d0ab5e9304e
satya-pradeep <gedelasatyapradeep@gmail.com>
2025-11-11 00:24:50 +0530

    Add files via upload

---
+++ arithmetical operations.py
@@ -0,0 +1,16 @@
+"""
+# Addition
+a=float(input("enter first number: "))
+b=float(input("enter second number: "))
+sum=a+b
+print(f"sum: {sum:.2f} ")
+
+"""
+#Division
+a=float(input("enter first number: "))
+b=float(input("enter second number: "))
+if b==0:
+    print('error')
+else:
+    d=a/b
+    print(f"division: {d:.2f}")
