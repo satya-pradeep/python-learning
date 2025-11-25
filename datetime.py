b4e67f6eb6e34f0f21b7996ccbd8b193c2b6fbe1
satya pradeep <gedelasatyapradeep@gmail.com>
2025-11-25 23:46:47 +0530

    Add datetime.py to display current date and time

---
+++ datetime.py
@@ -0,0 +1,18 @@
+import datetime
+x=datetime.datetime.now()
+print(x)
+
+import datetime
+y=datetime.datetime.now()
+print(y.year)
+print(y.month)
+print(y.day)
+print(y.strftime("%A"))
+print(y.strftime("%a"))
+print(y.strftime("%B"))
+print(y.strftime("%b"))
+print(y.strftime("%d"))
+print(y.strftime("%y"))
+print(y.strftime("%H"))
+print(y.strftime("%I"))
+print(y.strftime("%p"))
